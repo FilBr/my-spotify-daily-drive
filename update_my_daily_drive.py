ca9f6ca17efe5bc9883cbb19168178d656f1f150
Filippo Barba <filb@nuuday.dk>
2024-06-26 10:46:08 +0200

    cleaning up code

--- update_my_daily_drive.py
+++ update_my_daily_drive.py
@@ -1,10 +1,12 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
-from datetime import datetime, timedelta
 import os
+from datetime import datetime, timedelta
+
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
+
 
 def main():
     scope = "playlist-modify-private"
@@ -14,7 +16,7 @@ def main():
     dailydrive = sp.playlist(spotify_daily_drive_id)
 
     # Get the tracks from the playlist
-    tracks = sp.playlist(spotify_daily_drive_id)
+    tracks = dailydrive["tracks"]["items"][1:]
 
     # Podcasts IDs
     the_essential_podcast_id = os.environ["THE_ESSENTIAL_PODCAST_ID"]
@@ -34,21 +36,21 @@ def main():
 
     clean_playlist = []
     # Clean playlist
-    for track in tracks["tracks"]["items"][1:]:
+    for track in tracks:
         if track["track"]["uri"].startswith("spotify:track:"):
             clean_playlist.append(track["track"])
 
     podcast_idx = 0
     for idx, podcast in enumerate(podcasts):
-        clean_playlist.insert(
-            min(len(clean_playlist), podcast_idx), 
-            podcast
-        )
+        clean_playlist.insert(min(len(clean_playlist), podcast_idx), podcast)
         podcast_idx += (idx + 1) + 4
 
     my_daily_drive_id = os.environ["MY_DAILY_DRIVE_ID"]
-    sp.playlist_replace_items(my_daily_drive_id, [track["uri"] for track in clean_playlist])
+    sp.playlist_replace_items(
+        my_daily_drive_id, [track["uri"] for track in clean_playlist]
+    )
     print(f"[{datetime.now()}] - Daily Drive updated! 🎶🎧")
+
 
 if __name__ == "__main__":
     try:
